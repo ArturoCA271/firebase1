a342245687241a5a5e5cdb0e221da2a9deac7bc0
ArturoCA271 <64658934+ArturoCA271@users.noreply.github.com>
2020-07-08 11:43:10 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,25 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-<<<<<<< HEAD
-    apiKey: "AIzaSyCM7hlzlg_71ZHY9TpPxdQ6kemBKuxZV2E",
-    authDomain: "loggin-ed936.firebaseapp.com",
-    databaseURL: "https://loggin-ed936.firebaseio.com",
-    projectId: "loggin-ed936",
-    storageBucket: "loggin-ed936.appspot.com",
-    messagingSenderId: "710111793626",
-    appId: "1:710111793626:web:0e18f37aeff5e2281470e3",
-    measurementId: "G-5PVLT2LDTL"
-=======
-    apiKey: "AIzaSyBfbz884KLwog1f1cckI1aFrDf2Hw6fPIk",
-    authDomain: "tabs2-3fb37.firebaseapp.com",
-    databaseURL: "https://tabs2-3fb37.firebaseio.com",
-    projectId: "tabs2-3fb37",
-    storageBucket: "tabs2-3fb37.appspot.com",
-    messagingSenderId: "507794303981",
-    appId: "1:507794303981:web:93bf876efe1ce6823d95e4",
-    measurementId: "G-DS2XKZBFNZ"
->>>>>>> versionqr
+
   }
 
 };
